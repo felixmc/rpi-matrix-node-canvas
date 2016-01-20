5e0f69ceee3cd2df2c358567f590f0342fde3ce8
Felix Milea-Ciobanu <felixmc@users.noreply.github.com>
2016-01-20 13:36:58 -0700

    nodejs code cleanup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var WIDTH = 32;
+var PORT  = 3000;
 
 var express = require('express');
 var app     = express();
@@ -12,24 +13,30 @@ var matrix    = new LedMatrix(WIDTH);
 
 var Color = require('onecolor');
 
+
 app.use(express.static('public'));
 
 io.on('connection', function(socket) {
+	
   socket.on('draw', function(data) {
-		var x = data.index % WIDTH;
-		var y = Math.floor(data.index / WIDTH);
-		var color = Color(data.color);
+    var x = data.index % WIDTH;
+    var y = Math.floor(data.index / WIDTH);
+    var color = Color(data.color);
 
-		matrix.setPixel(WIDTH - y, x, color.red() * 255, color.green() * 255, color.blue() * 255);
+    matrix.setPixel(x, y, color.red() * 255, color.green() * 255, color.blue() * 255);
+    
+    // my LED matrix is setup at 90deg CCW, so I have to do some math magic on the coordinates so things work properly
+    // matrix.setPixel(WIDTH - y, x, color.red() * 255, color.green() * 255, color.blue() * 255);
 
     console.log('draw data:', data);
   });
 
-	socket.on('clear', function() {
-		matrix.fill(0, 0, 0);
-	});
+  socket.on('clear', function() {
+    matrix.fill(0, 0, 0);
+  });
+
 });
 
-server.listen(3000, function() {
-  console.log('listening on *:3000');
+server.listen(PORT, function() {
+  console.log('listening on *:' + PORT);
 });
